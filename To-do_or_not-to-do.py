9741ef3784ef2a07439862def424dc4606229f2c
Rudolf Daniel <rudolf.daniel@outlook.com>
2017-12-14 16:03:57 +0100

    remover added

--- To-do_or_not-to-do.py
+++ To-do_or_not-to-do.py
@@ -10,25 +10,44 @@ rtfm = ("Command Line Todo application" "\n"
     "Command line arguments:" "\n"
     " -l   Lists all the tasks" "\n"
     " -a   Adds a new task" "\n"
-    " -r   Removes an task" "\n"
-    " -c   Completes an task")
+    " -r   Removes a task" "\n"
+    " -c   Completes a task")
 
 def lister():
     todolist = open("tasklist.txt", "r")
     list_of_tasks = todolist.readlines()
-    for i in range(len(list_of_tasks)):
-        print(str(i+1) + " - " + list_of_tasks[i].strip())
+    if len(list_of_tasks) == 0:
+        print("No todos for today! :)")
+    else:
+        for i in range(len(list_of_tasks)):
+            print(str(i+1) + " - " + list_of_tasks[i].strip())
+    todolist.close()
+
+def task_adder(new_text):
+    todolist = open("tasklist.txt", "a")
+    todolist.write("\n" + new_text)
+    todolist.close()
+
+def task_remover(remover):
+    todolist = open("tasklist.txt", "r")
+    list_of_task = todolist.readlines()
+    list_of_task = list_of_task[0:int(remover)-1] + list_of_task[int(remover):]
+    todolist.close()
+    todolist = open("tasklist.txt", "w")
+    for i in range(len(list_of_task)):
+        todolist.write(list_of_task[i].strip() + "\n")
     todolist.close()
 
 if len(sys.argv) == 1:
     print(rtfm)
-if len(sys.argv) == 2:
+elif len(sys.argv) == 2:
     if sys.argv[1] == "-l":
         lister()
-    elif sys.argv[1] == "-a":
-        print("You wanted to add a new task.")
+elif len(sys.argv) == 3:
+    if sys.argv[1] == "-a":
+        task_adder(sys.argv[2])
     elif sys.argv[1] == "-r":
-        print("You wanted to remove a task.")
+        task_remover(sys.argv[2])
     elif sys.argv[1] == "-c":
         print("You wanted to complete a task.")
     else:
